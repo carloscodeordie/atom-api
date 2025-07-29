2e6771283d8738fec3376fc873d368040124578b
Carlos Macias <lmaciasgonzalez@gmail.com>
2025-07-29 13:27:35 -0600

    feat(config): fix configuration to deployment

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as admin from 'firebase-admin';
-import * as serviceAccount from './database-service-account.json';
+import * as path from 'path';
 
 @Injectable()
-export class DatabaseService {
+export class DatabaseService implements OnModuleInit {
   private firestore: FirebaseFirestore.Firestore;
 
-  constructor() {
+  onModuleInit() {
+    const serviceAccountPath = path.resolve(
+      __dirname,
+      '../config/firebase-service-account.json',
+    );
+
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+      credential: admin.credential.cert(serviceAccountPath),
     });
+
     this.firestore = admin.firestore();
   }
 
